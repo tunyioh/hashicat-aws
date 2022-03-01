1336f1b84008fbcc1d6f80c8dfad75104f405fea
ohtunyi <ohtunyi@gmail.com>
2022-03-01 13:41:12 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.14.1"
   # insert the 5 required variables here
-  bucket_prefix = prefix
+  bucket_prefix = var.prefix
 }
